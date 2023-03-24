e2613a50946c91aa2d7fa2e97af1878d55658976
Pham Tien Chung <phamtienchung2@gmail.com>
2023-03-24 13:56:32 +0700

    modified logic

--- src/card/card.js
+++ src/card/card.js
@@ -1,26 +1,55 @@
-import { useState } from 'react';
+import { useImperativeHandle, useState, forwardRef } from 'react';
 import profilePic from '../Star_Wars_Logo.svg.png'
 import './card.css';
-export default function Card({ ship, type, speedCb, costCb, passCb, filmCb }) {
+export const Card = forwardRef(({ ship, type, speedCb, costCb, passCb, filmCb }, ref) => {
     const [isShowSpeed, setIsShowSpeed] = useState(false);
     const [isShowCost, setIsShowCost] = useState(false);
     const [isShowPass, setIsShowPass] = useState(false);
     const [isShowFilm, setIsShowFilm] = useState(false);
+    const [selectedSpeed, setSelectedSpeed] = useState('');
+    const [selectedCost, setSelectedCost] = useState('');
+    const [selectedPass, setSelectedPass] = useState('');
+    const [selectedFilm, setSelectedFilm] = useState('');
+    useImperativeHandle(ref, () => ({
+        refreshCardState() {
+            setIsShowSpeed(false);
+            setIsShowCost(false);
+            setIsShowPass(false);
+            setIsShowFilm(false);
+            setSelectedSpeed('');
+            setSelectedCost('');
+            setSelectedPass('');
+            setSelectedFilm('');
+        }
+    }))
+
     function compareSpeed() {
         setIsShowSpeed(true);
-        speedCb();
+        setSelectedSpeed('selected');
+        setTimeout(()=>{
+            speedCb();
+        },1000)
     }
     function compareCost() {
         setIsShowCost(true);
-        costCb();
+        setSelectedCost('selected');
+        setTimeout(()=>{
+            costCb();
+        },1000)
     }
     function comparePass() {
         setIsShowPass(true);
-        passCb();
+        setSelectedPass('selected');
+        setTimeout(()=>{
+            passCb();
+        },1000)
     }
     function compareFilm() {
         setIsShowFilm(true);
-        filmCb();
+        setSelectedFilm('selected');
+        setTimeout(()=>{
+            filmCb();
+        },1000)
     }
     return (
         <div className="cardHolder">
@@ -29,19 +58,19 @@ export default function Card({ ship, type, speedCb, costCb, passCb, filmCb }) {
             </div>
             <img alt="" src={profilePic} />
             <div className="content">
-                <div onClick={compareSpeed}>Max speed: {
+                <div className={selectedSpeed || ''} onClick={compareSpeed}>Max speed: {
                     (type === 'user' && !isShowSpeed) ? <p>?</p> : <p>{ship?.max_atmosphering_speed}</p>
                 }</div>
-                <div onClick={compareCost}>Credist cost: {
+                <div className={selectedCost || ''} onClick={compareCost}>Credist cost: {
                     (type === 'user' && !isShowCost) ? <p>?</p> : <p>{ship?.cost_in_credits}</p>
                 }</div>
-                <div onClick={comparePass}>Passengers: {
+                <div className={selectedPass || ''} onClick={comparePass}>Passengers: {
                     (type === 'user' && !isShowPass) ? <p>?</p> : <p>{ship?.passengers}</p>
                 }</div>
-                <div onClick={compareFilm}>Film Apperances: {
+                <div className={selectedFilm || ''} onClick={compareFilm}>Film Apperances: {
                     (type === 'user' && !isShowFilm) ? <p>?</p> : <p>{ship?.films?.length}</p>
                 }</div>
             </div>
         </div>
     )
-}
+})
